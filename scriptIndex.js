6cfe38c4185b5a269f224a8d0f678cd66f78c6f0
Sixeyes198 <yossiguadie@icloud.com>
2024-01-15 14:45:48 +0200

    update

--- scriptIndex.js
+++ scriptIndex.js
@@ -11,3 +11,12 @@ btnClose.addEventListener(`click`, hide);
 function hide() {
   wrapper.style.display = `none`;
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  const menuIcon = document.getElementById("menu-icon");
+  const navigation = document.getElementById("navigation");
+
+  menuIcon.addEventListener("click", function () {
+    navigation.classList.toggle("active");
+  });
+});
